8dbd4eb90dc977e0ca1cfe457f5d7a9162d11238
Hanio90 <hanio90@gmail.com>
2018-02-08 15:01:39 -0500

    Added validation class

--- Midterm_BeerStorePOS/POSApp.cs
+++ Midterm_BeerStorePOS/POSApp.cs
@@ -17,14 +17,14 @@ namespace Midterm_BeerStorePOS
             if (UserInput == "1")
             {
                 Console.Clear();
-                List <Beer> BeerSelection = DisplaySelection();
+                List<Beer> BeerSelection = DisplaySelection();
                 AddToCart(BeerSelection, CartItems);
             }
             if (UserInput == "2")
             {
                 //display cart
             }
-            
+
         }
 
         private void PrintMenu()
@@ -42,18 +42,18 @@ namespace Midterm_BeerStorePOS
         {
             List<Beer> BeerSelection = new List<Beer>();
 
-            foreach(string b in Beer.GetBeer("../../ProductList.txt"))
+            foreach (string b in Beer.GetBeer("../../ProductList.txt"))
             {
                 string[] temp = b.Split(',');
-                BeerSelection.Add(new Beer(temp[0],temp[1] ,temp[2] ,temp[3]));
+                BeerSelection.Add(new Beer(temp[0], temp[1], temp[2], temp[3]));
             }
 
             Console.WriteLine($"{"Beer",-20}{"Style",-10}{"Package",-20}{"Price",-10}");
             //continue header format
 
-            for (int i = 0; i< BeerSelection.Count; i++)
+            for (int i = 0; i < BeerSelection.Count; i++)
             {
-                Console.WriteLine($"{i+1,-4}{BeerSelection[i].BeerName,-20}{BeerSelection[i].BeerStyle,-10}{BeerSelection[i].BeerDescription,-20}" +
+                Console.WriteLine($"{i + 1,-4}{BeerSelection[i].BeerName,-20}{BeerSelection[i].BeerStyle,-10}{BeerSelection[i].BeerDescription,-20}" +
                     $"{BeerSelection[i].BeerPrice,-10}");
             }
             return BeerSelection;
@@ -61,17 +61,17 @@ namespace Midterm_BeerStorePOS
 
         private void AddToCart(List<Beer> BeerSelection, List<Cart> CartItems)
         {
-            
+
             Console.WriteLine("Please select item to add to cart:");
-            int ItemInput = int.Parse(Console.ReadLine()) -1 ;
+            int ItemInput = int.Parse(Console.ReadLine()) - 1;
             Console.WriteLine("Please select quantity:");
             double QuantityInput = double.Parse(Console.ReadLine());
             double LineSubtotal = QuantityInput * double.Parse(BeerSelection[ItemInput].BeerPrice);
 
-           CartItems.Add(new Cart(BeerSelection[ItemInput].BeerName,BeerSelection[ItemInput].BeerStyle,
-            BeerSelection[ItemInput].BeerDescription, BeerSelection[ItemInput].BeerPrice, QuantityInput, LineSubtotal));
-                
-           Console.WriteLine($"You've added{BeerSelection[ItemInput].BeerName}. The price is {BeerSelection[ItemInput].BeerPrice}. Line Subtotal: {LineSubtotal}");
+            CartItems.Add(new Cart(BeerSelection[ItemInput].BeerName, BeerSelection[ItemInput].BeerStyle,
+             BeerSelection[ItemInput].BeerDescription, BeerSelection[ItemInput].BeerPrice, QuantityInput, LineSubtotal));
+
+            Console.WriteLine($"You've added{BeerSelection[ItemInput].BeerName}. The price is {BeerSelection[ItemInput].BeerPrice}. Line Subtotal: {LineSubtotal}");
         }
     }
 }
--- Midterm_BeerStorePOS/Validation.cs
+++ Midterm_BeerStorePOS/Validation.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Midterm_BeerStorePOS
 {
     class Validation
     {
+
+        public static string validateSelection(string tempInput)
+        {
+            
+           // UserInput = Validation.validateSelection(UserInput);
+            while (true)
+            {
+             
+                if (Regex.IsMatch(tempInput, "([1-4])"))
+                {
+                    return tempInput;
+                }
+                Console.WriteLine("Please enter valid input (1-4)");
+                tempInput = Console.ReadLine();
+            }
+        }
+
+
+
+
+        public static int validateItem(int tempItemInput)
+        {
+
+            // UserInput = Validation.validateSelection(UserInput);
+            while (true)
+            {
+               // Validation.validateItem(ItemInput);
+                if (tempItemInput >=1 && tempItemInput <=12)
+                {
+                    return  tempItemInput;
+                }
+                Console.WriteLine("Please enter valid input (1-12)");
+                tempItemInput = int.Parse (Console.ReadLine());
+            }
+        }
+
+
     }
 }
